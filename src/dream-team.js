f7e82b12b559e6430be2f7555aeaaf63e0200b44
psyhramovnik1985 <psyhramovnik@rambler.ru>
2021-03-13 14:36:57 +0300

    feat: add logics in file dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {return false}
+  let mas = [];
+  let charCode;
+  members.map(func);
+  function func (e){
+    if (typeof e === 'string'){
+      charCode = e.trim().split('')[0].toUpperCase();
+      if (charCode.charCodeAt(0) >= 65 && charCode.charCodeAt(0) <= 90) {
+        mas.push(charCode);
+      }        
+    }      
+  }
+  return mas.sort().join('');
 };
