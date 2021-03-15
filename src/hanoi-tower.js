f47c8de7f2baec831afaf150900cb0c2272a6e3a
psyhramovnik1985 <psyhramovnik@rambler.ru>
2021-03-15 12:00:54 +0300

    feat: add logics in file hanio-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(_disksNumber, _turnsSpeed) {
+  let obj = {turns: null, seconds: null };
+  obj.turns = Math.pow(2,_disksNumber) - 1;
+  obj.seconds = Math.floor(obj.turns/(_turnsSpeed/3600));
+  return obj;
 };
