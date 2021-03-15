b47073dfdb5544b6b05459f545b829aadae060b9
psyhramovnik1985 <psyhramovnik@rambler.ru>
2021-03-15 13:25:56 +0300

    feat: add logic in file transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let mas = [];
+  let status = false;
+
+  if (toString.call(arr) !== "[object Array]") {
+    throw new Error("Error");
+  } 
+  arr.map(func);
+
+
+  function func (e,i,arMas) {
+    if (e !== '--double-prev' && e !=='--double-next' && e !=='--discard-prev' && e !=='--discard-next' && !status) {
+      mas.push(e);
+    }  else {
+      switch (e) {
+        case "--discard-prev":
+            if (arMas[i - 2] !== '--discard-next') {
+              mas.pop();
+            }
+              break;
+
+        case "--discard-next":
+            status = true;
+              break;
+
+        case "--double-prev":
+            if (arMas[i - 2] !== '--discard-next' && i !== 0) {
+              mas.push(mas[mas.length -1]);
+            }            
+              break;
+
+        case "--double-next":
+            if (i < arMas.length-1) {
+              mas.push(arMas[i+1]);
+            }
+            
+              break;
+
+        default:
+            status = false;
+          break;
+      }
+    } 
+  }
+
+  return mas;
 };
