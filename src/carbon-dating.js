3ef5d87e5b767a3721ee5c79fd869f479c398d37
psyhramovnik1985 <psyhramovnik@rambler.ru>
2021-03-16 18:08:00 +0300

    feat: add logics in file carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,8 +2,13 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+let result;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || parseFloat(sampleActivity) > MODERN_ACTIVITY || parseFloat(sampleActivity) <= 0 || !parseFloat(sampleActivity)) {
+    return false;
+  }
+ 
+  result = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) * HALF_LIFE_PERIOD / 0.693);
+  return result;
 };
